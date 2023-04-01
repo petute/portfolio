6c93fab595e66e7592026d5edd336832645950c8
petute <daniel.petutschnigg@gmail.com>
2023-04-01 17:43:15 +0200

    chore: add prettier

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  plugins: [require('prettier-plugin-tailwindcss')],
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: false,
+  arrowParens: 'avoid',
+  bracketSameLine: false,
+  endOfLine: 'lf',
+  proseWrap: 'always',
+  singleAttributePerLine: true
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,27 @@
 const theme = {
+  backgroundImage: {
+    me: "url('/me.webp')"
+  },
   colors: {
     neobrutalism: {
       green: '#B0CDAF',
       red: '#E47C84',
-      yellow: '#EED256',
+      yellow: '#EED256'
     }
   },
   boxShadow: {
     neobrutalism: '4px 4px 0 0 black'
   }
-  
 }
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './index.html',
-    './src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}',
+    './src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}'
   ],
   darkMode: 'class',
   theme: {
-    extend: {...theme},
+    extend: {...theme}
   },
-  plugins: [require("@kobalte/tailwindcss"),],
-};
+  plugins: [require('@kobalte/tailwindcss')]
+}
